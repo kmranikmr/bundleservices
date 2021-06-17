c580bd51f2e0022614a61b5b300b9a4cde5ded67
ubuntu <anilqmar@gmail.com>
2021-06-17 02:35:22 +0000

    added conversion

--- Services/FileUpload/FileUploadAPI/FileUploadService/Utils/FileHelpers.cs
+++ Services/FileUpload/FileUploadAPI/FileUploadService/Utils/FileHelpers.cs
@@ -76,6 +76,27 @@ namespace FileUploadService.Utils
             //    }
             //}
         }
+        public static bool RunConversion(string path, string filename)
+        {
+            Console.WriteLine("RunConversion");
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = "/usr/bin/python3";//cmd is full path to python.exe
+            start.Arguments = $"{path} {filename}";//args is path to .py file and any cmd line args
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            using (Process process = Process.Start(start))
+            {
+                Console.WriteLine("start process");
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    string result = reader.ReadToEnd();
+                    Console.Write(result);
+                    return true;
+                }
+               
+            }
+           
+        }
     }   
 
     public static class FileHelpers
